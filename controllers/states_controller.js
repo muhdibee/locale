8fc4d4af3e00c704dd92e74a95d1526c70c9c6c7
muhammad Ibrahim <muhdibee@gmail.com>
2023-07-06 03:57:48 +0100

    Sort results of querying all states using the name field.

--- controllers/states_controller.js
+++ controllers/states_controller.js
@@ -3,8 +3,8 @@ const {states} = require('../models/states_model');
 //GET states
 const get_states = async(req, res)=> {
     try {
-        const all_states = await states.find({}, {"no_of_lgas":0, "capital":0, "region":0, "population":0, "description":0, "__v":0, "_id":0});
-        return res.status(200).json({all_states})
+        const all_states = await states.find({}, {"no_of_lgas":0, "capital":0, "region":0, "population":0, "description":0, "__v":0, "_id":0}).sort("name");
+        return res.status(200).send(all_states)
     }
     catch(err){
         console.log("Error: ", err)
