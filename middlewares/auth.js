157df7e95f115041be92a1ed0b388e0900b455f1
muhammad Ibrahim <muhdibee@gmail.com>
2023-07-14 06:26:48 +0100

    Authenticate user.

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,5 +1,22 @@
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const {users} = require('../models/users_model');
 
-export const auth = (req, res, next) => {
 
+const auth = async(req, res, next) => {
+    const {api_key} = req.query;
+
+    try{
+        if(!api_key){
+            throw "api key is required.";
+        }
+        const user = await users.findOne({api_key});
+        req.user = user;
+        next()
+    }catch(err){
+        console.log(err)
+        res.status(400).json({"error": "Authentication error", "message": err})
+    }
 }
+
+module.exports = auth
--- routes/states_route.js
+++ routes/states_route.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const states_router = express.Router();
-const states_controller = require('../controllers/states_controller')
+const states_controller = require('../controllers/states_controller');
+const auth = require("../middlewares/auth")
 
 states_router.route("/")
-.get(states_controller.get_states);
+.get(auth, states_controller.get_states);
 
 
 module.exports = states_router
