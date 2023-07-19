fc0cdc8025417313f3dae70ad9e50540b092ca6c
muhammad Ibrahim <muhdibee@gmail.com>
2023-07-19 14:58:52 +0100

    Port update.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Import dependencies
+require("dotenv").config();
 const express = require("express");
 const logger = require("morgan");
 const cors = require('cors');
@@ -15,7 +16,7 @@ const users_router = require('./routes/users_route');
 
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // DB connection
 connect_to_database();
