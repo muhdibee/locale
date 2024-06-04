26a3eccd8f3d64c3254a471b1f50ea931d9cfbd7
Muhammad Ibrahim <muhdibee@gmail.com>
2024-06-04 17:03:10 +0100

    Handled 404 error in search controller.

--- controllers/search_controller.js
+++ controllers/search_controller.js
@@ -1,62 +1,86 @@
-const {regions} = require('../models/regions_model');
-const {states} = require('../models/states_model');
-const {local_gov_areas} = require('../models/local_gov_areas_model');
-
+const { regions } = require("../models/regions_model");
+const { states } = require("../models/states_model");
+const { local_gov_areas } = require("../models/local_gov_areas_model");
 
 // Search for a region function.
-const searchRegions = async(req, res, q) => {
-    try {
-        const region_data = await regions.findOne({ name: { $regex: new RegExp(`^${q}$`, 'i') } }, {"name":1, "states":1, "capital":1, "description":1, "_id":0 });
-        return res.status(200).send(region_data)
+const searchRegions = async (req, res, q) => {
+  try {
+    const region_data = await regions.findOne(
+      { name: { $regex: new RegExp(`^${q}$`, "i") } },
+      { name: 1, states: 1, capital: 1, description: 1, _id: 0 }
+    );
+    if (!region_data) {
+      return res.status(404).send(`'${q}' not found`);
     }
-    catch(err){
-        console.log("Error: ", err)
-    }
-
-}
-
-// Search for a state function.
-const searchStates = async(req, res, q) => {
-        try {
-        const state_data = await states.findOne({ name: { $regex: new RegExp(`^${q}$`, 'i') } }, {"name":1, "capital":1, "region":1, "population":1, "description":1, "no_of_lgas":1, "local_gov_areas":1,  "_id":0 });
-        return res.status(200).send(state_data)
-    }
-    catch(err){
-        console.log("Error: ", err)
-    }
-
-}
-
-
-// Search for a local government area function.
-const search_local_gov_areas = async(req, res, q) => {
-        try {
-        const local_gov_area_data = await local_gov_areas.findOne({ name: { $regex: new RegExp(`^${q}$`, 'i') } }, {"name":1, "state":1, "region":1, "postal_code":1, "population":1, "description":1, "_id":0 });
-        return res.status(200).send(local_gov_area_data)
-    }
-    catch(err){
-        console.log("Error: ", err)
-    }
-
-}
-
-
-
-// Search 
-const search = (req, res)=> {
-    const {category, q} = req.query;
-
-    if(category === "region"){
-        return searchRegions(req, res, q);
-    }else if(category === "state"){
-        return searchStates(req, res, q);
-    }else if(category === "local_gov_area"){
-        return search_local_gov_areas(req, res, q);
-    }
+    return res.status(200).send(region_data);
+  } catch (err) {
+    console.log("Error: ", err);
+  }
 };
 
+// Search for a state function.
+const searchStates = async (req, res, q) => {
+  try {
+    const state_data = await states.findOne(
+      { name: { $regex: new RegExp(`^${q}$`, "i") } },
+      {
+        name: 1,
+        capital: 1,
+        region: 1,
+        population: 1,
+        description: 1,
+        no_of_lgas: 1,
+        local_gov_areas: 1,
+        _id: 0,
+      }
+    );
+    if (!state_data) {
+      return res.status(404).send(`'${q}' not found`);
+    }
+    return res.status(200).send(state_data);
+  } catch (err) {
+    console.log("Error: ", err);
+  }
+};
 
+// Search for a local government area function.
+const search_local_gov_areas = async (req, res, q) => {
+  try {
+    const local_gov_area_data = await local_gov_areas.findOne(
+      { name: { $regex: new RegExp(`^${q}$`, "i") } },
+      {
+        name: 1,
+        state: 1,
+        region: 1,
+        postal_code: 1,
+        population: 1,
+        description: 1,
+        _id: 0,
+      }
+    );
+    if (!local_gov_area_data) {
+      return res.status(404).send(`'${q}' not found`);
+    }
+
+    return res.status(200).send(local_gov_area_data);
+  } catch (err) {
+    console.log("Error: ", err);
+  }
+};
+
+// Search
+const search = (req, res) => {
+  const { category, q } = req.query;
+
+  if (category === "region") {
+    return searchRegions(req, res, q);
+  } else if (category === "state") {
+    return searchStates(req, res, q);
+  } else if (category === "local_gov_area") {
+    return search_local_gov_areas(req, res, q);
+  }
+};
 
 module.exports = {
-    search
-}
+  search,
+};
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,59 +1,55 @@
-require('dotenv').config();
-const jwt = require('jsonwebtoken');
-const {users} = require('../models/users_model');
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const { users } = require("../models/users_model");
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
-
 //GET users
-const get_users = async(req, res)=> {
-    try {
-        const all_users = await users.find({});
-        return res.status(200).json(all_users)
-    }
-    catch(err){
-        console.log("Error: ", err)
-    }
+const get_users = async (req, res) => {
+  try {
+    const all_users = await users.find({});
+    return res.status(200).json(all_users);
+  } catch (err) {
+    console.log("Error: ", err);
+  }
 };
 
 //SignUp a user
-const signUp_user = async(req, res)=> {
-    try {
-        const {first_name, last_name, email } = req.body;
-        const userExist = await users.findOne({email: new RegExp(`^${email}$`, 'i') });
+const signUp_user = async (req, res) => {
+  try {
+    const { first_name, last_name, email } = req.body;
+    const userExist = await users.findOne({
+      email: new RegExp(`^${email}$`, "i"),
+    });
 
-        // Check if user exist.
-        if(userExist !== null){
-            throw {"message": `A user with email '${email}' already exist. please use another email address.`}
+    // Check if user exist.
+    if (userExist !== null) {
+      throw {
+        message: `A user with email '${email}' already exist. please use another email address.`,
+      };
+    } else {
+      const api_key = jwt.sign({ first_name, last_name, email }, JWT_SECRET);
 
-        }else{
-
-            const api_key = jwt.sign({first_name, last_name, email}, JWT_SECRET)
-            
-            const user = await users.create({
-                first_name,
-                last_name,
-                email,
-                api_key
-            });
-            return res.status(200).json({
-                "first_name":user.first_name, 
-                "last_name":user.last_name,
-                "email": user.email,
-                "api_key": user.api_key
-            })
-        }
+      const user = await users.create({
+        first_name,
+        last_name,
+        email,
+        api_key,
+      });
+      return res.status(201).json({
+        first_name: user.first_name,
+        last_name: user.last_name,
+        email: user.email,
+        api_key: user.api_key,
+      });
     }
-    catch(err){
-        console.log("Error: ", err)
-        res.status(400).send( err)
-
-    }
+  } catch (err) {
+    console.log("Error: ", err);
+    res.status(400).send(err);
+  }
 };
 
-
-
 module.exports = {
-    get_users,
-    signUp_user,
-}
+  get_users,
+  signUp_user,
+};
